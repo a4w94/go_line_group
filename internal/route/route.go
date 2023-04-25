5072d92111dace6b55a7555c513b85ac5448ba3d
Terry Hsiesh <lin020488995@gmail.com>
2023-04-25 15:01:57 +0800

    line test

--- internal/route/route.go
+++ internal/route/route.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+	v1 "go_line_group/api/v1"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,14 +14,14 @@ func InitRouter() *gin.Engine {
 	// router.Use(gin.BasicAuth(gin.Accounts{
 	// 	"admin": "123456",
 	// }))
-	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.LoadHTMLGlob("/templates/*.tmpl.html")
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	//router.Any("/callback", v1.LineReply)
+	router.Any("/callback", v1.LineReply)
 
 	// This is just sample code.
 	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
